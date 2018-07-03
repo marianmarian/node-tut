1b6260c3f57fab8d480bdb72692cd3b877b15dce
marianmarian <fmmircea@yahoo.com>
2018-07-03 21:45:48 +0300

    Module si declaratie require

--- Tut2/globalObject/app.js
+++ Tut2/globalObject/app.js
@@ -7,3 +7,21 @@ setTimeout(function() {
   console.log("Au trecut 3 secunde");
 }, 3000);
 */
+
+/*
+var time = 0;
+
+var timer = setInterval(function() {
+  time += 2;
+  console.log("Au trecut " + time + " secunde.");
+  if (time > 20) {
+    clearInterval(timer);
+  }
+}, 2000);
+*/
+
+
+/*
+console.log(__dirname);
+console.log(__filename);
+*/
---
+++ Tut2/modulesRequire/count.js
@@ -0,0 +1,3 @@
+var counter = function(arr) {
+  return  "Sunt " + arr.length + " elemente in array.";
+}
