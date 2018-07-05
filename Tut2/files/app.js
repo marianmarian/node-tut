f106d0e4ac027f407343a59df3602ee6ddc43c6a
marianmarian <fmmircea@yahoo.com>
2018-07-05 12:11:09 +0300

    Scriere-citire fisiere sync si async

--- Tut2/files/app.js
+++ Tut2/files/app.js
@@ -1,5 +1,23 @@
 var fs = require("fs");
 
+/*
 var readMe = fs.readFileSync("readMe.txt", "utf8");
 
 console.log(readMe);
+*/
+/*
+var readMe = fs.readFileSync("readMe.txt", 'utf8');
+fs.writeFileSync("writeMe.txt", readMe);
+*/
+
+var readMe = fs.readFile("readMe.txt", "utf8", function(err, data) {
+  fs.writeFile("writeMe.txt", data, (err) => {
+    if (err) {
+      console.log("Eroare");
+    } else {
+      console.log("Continutul a fost salvat");
+    }
+  });
+});
+
+console.log("test");
